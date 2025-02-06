1197e55ac5a9a722fdf3c454ce9c173bd8581f68
othman4dev <othmandevsup@gmail.com>
2025-02-06 17:10:52 +0100

    Updating the buildspec file to reslove the problem

--- src/firestore/firestore.service.ts
+++ src/firestore/firestore.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import serviceAccount from '../config/firestore/secret.json';
 import { ConfigService } from '@nestjs/config';
 import { FieldValue } from 'firebase-admin/firestore';
 import { DatabaseTables } from 'src/enums/database-tables.enum';
@@ -9,25 +10,9 @@ import { firestore } from 'firebase-admin';
 export class FirestoreService {
   private db: admin.firestore.Firestore;
 
-  private static serviceAccount = {
-    type: process.env.FIRESTORE_TYPE,
-    project_id: process.env.FIRESTORE_PROJECT_ID,
-    private_key_id: process.env.FIRESTORE_PRIVATE_KEY_ID,
-    private_key: process.env.FIRESTORE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIRESTORE_CLIENT_EMAIL,
-    client_id: process.env.FIRESTORE_CLIENT_ID,
-    auth_uri: process.env.FIRESTORE_AUTH_URI,
-    token_uri: process.env.FIRESTORE_TOKEN_URI,
-    auth_provider_x509_cert_url:
-      process.env.FIRESTORE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIRESTORE_CLIENT_X509_CERT_URL,
-  };
-
   constructor(private configService: ConfigService) {
     admin.initializeApp({
-      credential: admin.credential.cert(
-        FirestoreService.serviceAccount as admin.ServiceAccount,
-      ),
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
       databaseURL: this.configService.get<string>('FIRESTORE_DATABASE_URL'),
     });
     this.db = admin.firestore();
