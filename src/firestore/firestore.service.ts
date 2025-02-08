44309e0e20dd33f8763379fed6a8d709dcd144d7
othman4dev <othmandevsup@gmail.com>
2025-02-08 01:00:27 +0100

    Fixing the firestore service

--- src/firestore/firestore.service.ts
+++ src/firestore/firestore.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import serviceAccount from '../config/firestore/secret.json';
 import { ConfigService } from '@nestjs/config';
 import { FieldValue } from 'firebase-admin/firestore';
 import { DatabaseTables } from 'src/enums/database-tables.enum';
@@ -8,37 +9,12 @@ import { firestore } from 'firebase-admin';
 @Injectable()
 export class FirestoreService {
   private db: admin.firestore.Firestore;
-  private readonly logger = new Logger(FirestoreService.name);
 
   constructor(private configService: ConfigService) {
-    if (!admin.apps.length) {
-      try {
-        const serviceAccount: any = {
-          type: 'service_account',
-          project_id: this.configService.get<string>('FIREBASE_PROJECT_ID'),
-          private_key_id: this.configService.get<string>('FIREBASE_PRIVATE_KEY_ID'),
-          private_key: this.configService.get<string>('FIREBASE_PRIVATE_KEY')?.replace(/\\n/g, '\n'),
-          client_email: this.configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-          client_id: this.configService.get<string>('FIREBASE_CLIENT_ID'),
-          auth_uri: this.configService.get<string>('FIREBASE_AUTH_URI'),
-          token_uri: this.configService.get<string>('FIREBASE_TOKEN_URI'),
-          auth_provider_x509_cert_url: this.configService.get<string>('FIREBASE_AUTH_CERT_URL'),
-          client_x509_cert_url: this.configService.get<string>('FIREBASE_CLIENT_CERT_URL'),
-        };
-
-        this.logger.debug('Initializing Firebase with config:', serviceAccount);
-        
-        admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount),
-          databaseURL: this.configService.get<string>('FIRESTORE_DATABASE_URL'),
-        });
-        
-        this.logger.log('Firebase initialized successfully');
-      } catch (error) {
-        this.logger.error('Error initializing Firebase:', error);
-        throw error;
-      }
-    }
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      databaseURL: this.configService.get<string>('FIRESTORE_DATABASE_URL'),
+    });
     this.db = admin.firestore();
   }
 
